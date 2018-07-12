e5dc259346809167d2e0ec893529868c26676fa1
Arkadiy A.Davydov <arkadiy.a.davydov@gmail.com>
2018-07-12 15:51:18 +0300

    chore(contract-deployer): add logging

--- contract-deployer/index.js
+++ contract-deployer/index.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const Web3 = require('web3');
 const Tx = require('ethereumjs-tx');
+const os = require('os');
 
 // ----------------------------------------------------------------------------
 // personal data
@@ -11,6 +12,7 @@ const Tx = require('ethereumjs-tx');
 const pubKey = '';
 const privKey = '';
 const httpProvider = '';
+const contractsToDeploy = 3;
 
 // ----------------------------------------------------------------------------
 // helper functions
@@ -22,7 +24,8 @@ async function asyncForLoop(count, callback) {
   }
 }
 
-async function sendTransaction(address, key, data, web3) {
+async function sendTransaction(params) {
+  const {address, key, data, web3, successLog, errorLog, addressLog} = params;
   const gasPrice = await web3.eth.getGasPrice();
   console.log('Gas price: ' + gasPrice);
   const nonce = await web3.eth.getTransactionCount(address);
@@ -40,14 +43,15 @@ async function sendTransaction(address, key, data, web3) {
   const tx = new Tx(rawTx);
   tx.sign(privateKey);
   const serializedTx = tx.serialize().toString('hex');
-  web3.eth.sendSignedTransaction('0x' + serializedTx)
+  await web3.eth.sendSignedTransaction('0x' + serializedTx)
     .on('receipt', receipt => {
-      console.log('receipt: ' + receipt);
-      fs.writeFileSync('receipt.json', JSON.stringify(receipt));
+      console.log('success');
+      successLog.write(JSON.stringify(receipt) + os.EOL);
+      addressLog.write(JSON.stringify(receipt.contractAddress).replace(/"/g, '') + os.EOL);
     })
     .on('error', (error) => {
-      console.log(error);
-      fs.writeFileSync('error.txt', error);
+      console.log('error');
+      errorLog.write(error + os.EOL);
     });
 }
 
@@ -58,10 +62,13 @@ async function sendTransaction(address, key, data, web3) {
 const web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider(httpProvider));
 const bytecode = fs.readFileSync('contracts.bytecode').toString();
+const successLog = fs.createWriteStream('log_success.txt', {flags: 'a'});
+const errorLog = fs.createWriteStream('log_error.txt', {flags: 'a'});
+const addressLog = fs.createWriteStream('log_address.txt', {flags: 'a'});
 
-asyncForLoop(1, async (num) => {
+asyncForLoop(contractsToDeploy, async num => {
   console.log('counter: ', num);
-  await sendTransaction(pubKey, privKey, bytecode, web3);
+  await sendTransaction({address: pubKey, key: privKey, data: bytecode, web3, successLog, errorLog, addressLog});
 });
 
 /* eslint-enable no-console, no-plusplus */
