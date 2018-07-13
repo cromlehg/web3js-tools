b9facea5df587d12589da159e25bbd2e27aa75d9
Arkadiy A.Davydov <arkadiy.a.davydov@gmail.com>
2018-07-13 14:06:01 +0300

    chore(contract-deployer): run transactions in parallel, update logging, update README.md

--- contract-deployer/README.md
+++ contract-deployer/README.md
@@ -1,7 +1,24 @@
-# How to use
-1. Put **contracts.bytecode** in one folder with **index.js**
-2. Change "Personal data" section in **index.js** file: enter your **public key**, **private key** and your **infura link**
-3. Run **npm i**
-4. Run **node index.js**
+# BlockWit Contract Deployer
+Script for contract deployment.
+Calculates gas price depending on **gas price oracle**.  
+Use `minAllowableGasPrice` and `maxAllowableGasPrice` to specify the acceptable gas price range.
+## How to run:
+1. Place **contracts.bytecode** in the same folder as **index.js**
+2. Change **Personal data** section in **index.js** file:
+  * `publicKey` - your **public key**
+  * `privateKey` - **private key** corresponding to the **public key**
+  * `httpProvider` - **infura link** with your personal token
+  * `contractsToDeploy` - amount of contracts that you need to deploy
+  * `minAllowableGasPrice` - minimum allowable gas price in **Wei**
+  * `maxAllowableGasPrice` - maximum allowable gas price in **Wei**
+3. Run `npm i`
+4. Run `node index.js`
+5. Wait until the script finishes
 
-Tested with Node v9.5.0
+## Output:
+Script generates 3 files:
+* `log_success.txt` - transactions that have completed successfully
+* `log_error.txt` - transactions that returned an error
+* `log_address.txt` - addresses of successfully deployed contracts
+
+Tested with **Node v9.5.0**
--- contract-deployer/index.js
+++ contract-deployer/index.js
@@ -9,63 +9,91 @@ const os = require('os');
 // personal data
 // ----------------------------------------------------------------------------
 
-const pubKey = '';
-const privKey = '';
+const publicKey = '';
+const privateKey = '';
 const httpProvider = '';
 const contractsToDeploy = 3;
+const minAllowableGasPrice = 0;
 const maxAllowableGasPrice = 1000000000;
 
 // ----------------------------------------------------------------------------
 // helper functions
 // ----------------------------------------------------------------------------
 
-async function asyncForLoop(count, callback) {
-  for (let index = 0; index < count; index++) {
-    await callback(index);
-  }
+function dec2hex(num) {
+  return '0x' + Number(num).toString(16).toUpperCase();
+}
+
+function createLogStream() {
+  const successStream = fs.createWriteStream('log_success.txt', {flags: 'a'});
+  const errorStream = fs.createWriteStream('log_error.txt', {flags: 'a'});
+  const addressStream = fs.createWriteStream('log_address.txt', {flags: 'a'});
+  return () => {
+    function Log() {
+      let result = '';
+      this.write = string => {
+        result = result + string + os.EOL;
+      };
+      this.success = receipt => {
+        this.write('Success: Transaction successfully mined:');
+        this.write(JSON.stringify(receipt));
+        this.write('--------------------------------------------------------------------------------');
+        successStream.write(result);
+        addressStream.write(JSON.stringify(receipt.contractAddress).replace(/"/g, '') + os.EOL);
+      };
+      this.error = error => {
+        this.write(error);
+        this.write('--------------------------------------------------------------------------------');
+        errorStream.write(result);
+      };
+    }
+    return new Log();
+  };
 }
 
 async function sendTransaction(params) {
-  const {num, address, key, data, web3, successLog, errorLog, addressLog, maxGasPrice} = params;
-  let logString = '';
-  logString = logString + 'Transaction #' + num + os.EOL;
+  const {num, minGasPrice, maxGasPrice, address, key, data, web3, logStream} = params;
+  const nonce = params.nonce + num;
+  const log = logStream();
+  log.write('Transaction #' + num);
+  log.write('Nonce: ' + nonce);
+  const gasLimit = await web3.eth.estimateGas({from: address, data});
+  log.write('Gas limit: ' + gasLimit);
   let gasPrice = await web3.eth.getGasPrice();
-  logString = logString + 'Median gas price: ' + gasPrice + os.EOL;
   if (gasPrice > maxGasPrice) {
     gasPrice = maxGasPrice;
-    logString = logString + 'Median gas price exceeds the maximum allowable. Set gas price to: ' + gasPrice + os.EOL;
+    log.write('Gas price oracle exceeds the maximum allowable. Setting gas price to:  ' + gasPrice);
+  } else if (gasPrice < minGasPrice) {
+    gasPrice = minGasPrice;
+    log.write('Gas price oracle is below the minimum allowable. Setting gas price to: ' + gasPrice);
+  } else {
+    log.write('Gas price oracle is in the acceptable range. Setting gas price to:    ' + gasPrice);
   }
-  const nonce = await web3.eth.getTransactionCount(address);
-  logString = logString + 'Nonce: ' + nonce + os.EOL;
-  let gasLimit = await web3.eth.estimateGas({from: address, data});
-  logString = logString + 'Gas limit: ' + gasLimit + os.EOL;
   const rawTx = {
-    nonce,
-    gasLimit,
-    gasPrice,
+    nonce: dec2hex(nonce),
+    gasLimit: dec2hex(gasLimit),
+    gasPrice: dec2hex(gasPrice),
     data,
     from: address
   };
   const tx = new Tx(rawTx);
   tx.sign(Buffer.from(key, 'hex'));
   const serializedTx = tx.serialize().toString('hex');
-  console.log('Sending transaction #' + num);
   try {
     await web3.eth.sendSignedTransaction('0x' + serializedTx).on('receipt', receipt => {
-      logString = logString + 'Success: Transaction successfully mined:' + os.EOL;
-      logString = logString + JSON.stringify(receipt) + os.EOL;
-      logString = logString + '---------------------------------------' + os.EOL;
-      successLog.write(logString);
-      addressLog.write(JSON.stringify(receipt.contractAddress).replace(/"/g, '') + os.EOL);
-      console.log('Success');
-      console.log('-------------------------');
+      log.success(receipt);
     });
   } catch (e) {
-    logString = logString + e + os.EOL;
-    logString = logString + '---------------------------------------' + os.EOL;
-    errorLog.write(logString);
-    console.log('Error');
-    console.log('-------------------------');
+    log.error(e);
+  }
+}
+
+async function sendTransactions(params) {
+  const {address, key, minGasPrice, maxGasPrice, count, data, web3} = params;
+  const logStream = createLogStream();
+  const nonce = await web3.eth.getTransactionCount(address);
+  for (let num = 0; num < count; num++) {
+    sendTransaction({num, nonce, address, key, data, web3, minGasPrice, maxGasPrice, logStream});
   }
 }
 
@@ -75,14 +103,19 @@ async function sendTransaction(params) {
 
 const web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider(httpProvider));
-const bytecode = fs.readFileSync('contracts.bytecode').toString();
-const successLog = fs.createWriteStream('log_success.txt', {flags: 'a'});
-const errorLog = fs.createWriteStream('log_error.txt', {flags: 'a'});
-const addressLog = fs.createWriteStream('log_address.txt', {flags: 'a'});
-
-asyncForLoop(contractsToDeploy, async num => {
-  console.log('counter: ', num);
-  await sendTransaction({address: pubKey, key: privKey, data: bytecode, web3, successLog, errorLog, addressLog});
-});
+const data = fs.readFileSync('contracts.bytecode').toString();
+try {
+  sendTransactions({
+    address: publicKey,
+    key: privateKey,
+    count: contractsToDeploy,
+    minGasPrice: minAllowableGasPrice,
+    maxGasPrice: maxAllowableGasPrice,
+    data,
+    web3
+  });
+} catch (e) {
+  console.log(e);
+}
 
 /* eslint-enable no-console, no-plusplus */
